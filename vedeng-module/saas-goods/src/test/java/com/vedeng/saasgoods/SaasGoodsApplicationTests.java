978658fb7b31a541138f4b7a2d4c736a50b927ff
bert.jiao <1234qwer>
2018-11-29 13:15:22 +0800

    springboot多模块引入

---
+++ vedeng-module/saas-goods/src/test/java/com/vedeng/saasgoods/SaasGoodsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.vedeng.saasgoods;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SaasGoodsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
---
+++ vedeng-module/saas-sale/src/main/java/com/vedeng/saassale/SaasSaleApplication.java
@@ -0,0 +1,12 @@
+package com.vedeng.saassale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaasSaleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaasSaleApplication.class, args);
+	}
+}
